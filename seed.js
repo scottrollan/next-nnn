b68412c0b138df99c6a5ce536ef70314200b071d
Nathan Sierra <arreisnahtan@gmail.com>
2019-10-11 16:56:52 -0400

    moves seed files

--- seed.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Plant = require('./mongoose/Plant');
-const plants = require('./plantArray2.js');
-
-mongoose.connect("mongodb://localhost/nearlyNative", { useNewUrlParser: true });
-
-
-plants.forEach(plant => Plant.create(plant, (err) => { if(err){console.log(err)}}))
